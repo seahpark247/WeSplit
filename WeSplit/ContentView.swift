06284aecf0259dee4becf648531fbc769179405f
seahpark247 <famousjedo@gmail.com>
2025-05-20 08:09:34 -0500

    refactor(view): Edit code of background

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -41,41 +41,38 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                Text("").frame(maxWidth: .infinity, maxHeight: .infinity).background(.green.gradient).ignoresSafeArea()
-                
-                Form {
-                    Section("How much money"){
-                        TextField("Money", value: $money, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .keyboardType(.decimalPad)
-                            .focused($isFocused)
-                        
-                        Picker("Number of people", selection: $people) {
-                            ForEach(2..<100){
-                                Text("\($0) people")
-                            }
+            Form {
+                Section("How much money"){
+                    TextField("Money", value: $money, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    
+                    Picker("Number of people", selection: $people) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
                         }
                     }
-                    
-                    Section("Tip Percentages"){
-                        Picker("Tip percentages", selection: $tipPercentage) {
-                            ForEach(tipPercentages, id: \.self) {
-                                Text($0, format: .percent)
-                            }
-                        }.pickerStyle(.segmented)
-                    }
-                    
-                    Section("Total"){
-                        Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .TipWarningStyle(tipPercentage)
-                    }
-        
-                    Section("Split"){
-                        Text(String(format: "%.2f", split) + " \(Locale.current.currency?.identifier ?? "USD")")
-                    }
+                }
+                
+                Section("Tip Percentages"){
+                    Picker("Tip percentages", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("Total"){
+                    Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .TipWarningStyle(tipPercentage)
+                }
+    
+                Section("Split"){
+                    Text(String(format: "%.2f", split) + " \(Locale.current.currency?.identifier ?? "USD")")
                 }
             }
             .navigationTitle("WeSplit7")
+            .background(.green.gradient)
             .toolbar {
                 // 네비게이션 타이틀 붙이는 곳에, 툴바도 같이 붙인다. 같은 라인이니까!
                 if isFocused {
@@ -84,8 +81,7 @@ struct ContentView: View {
                     }
                 }
             }
-        }
-        .scrollContentBackground(.hidden)
+        }.scrollContentBackground(.hidden)
     }
     
 }
